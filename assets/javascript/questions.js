07bf78bc353d3ea1c8ae3255f3c4514ae99670f9
avnoto <alexandravnoto@gmail.com>
2020-04-25 17:21:01 -0700

    created function for getting current/next questions

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,27 +1,20 @@
 const questionsArr = [
     {
         question: "How old is the universe in years?",
-        choices: [
-            "4.6 billion years old",
-            "13.8 billion years old",
-            "10.3 trillion years old",
-            "1.2 million years old"
-        ],
-        answer: "13.8 billion years old"
-    
+        choiceA:  "4.6 billion years old",
+        choiceB:  "13.8 billion years old",
+        choiceC:  "10.3 trillion years old",
+        choiceD:  "1.2 million years old",
+        correct:   "B"
     },
     {
         question: "How many planets are in the Solar System?",
-        choices: [
-            "10",
-            "9",
-            "8",
-            "0"
-        ],
-        answer: "8"
-    
+        choiceA: "10",
+        choiceB: "9",
+        choiceC: "8",
+        choiceD: "1",
+        correct: "C"
     } 
 
 ];
 
-console.log(questionsArr[1].answer);
--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -1,16 +1,66 @@
-var qLead = document.getElementById("lead");
+questionsArr = []
+let lastQuestionIndex = questionsArr.length-1;
+let currrentQuestionIndex = 0;
+let qLead = document.getElementById("lead");
+const choice1 = document.getElementById("A");
+const choice2 = document.getElementById("B");
+const choice3 = document.getElementById("C");
+const choice4 = document.getElementById("D");
+let progress = document.getElementById("progress");
+let counter = document.getElementById("counter");
+
+
+function getQuestion() {
+    document.getElementById("lead1").classList.remove("hide");
+    let q = questionsArr[currrentQuestionIndex];
+    qLead.innerHTML = q.question;
+    choice1.innerHTML = q.choiceA;
+    choice2.innerHTML = q.choiceB;
+    choice3.innerHTML = q.choiceC;
+    choice4.innerHTML = q.choiceD;
+}
+
+
+// function answerIsCorrect() {
+//     document.getElementById(currrentQuestionIndex).style.backgroundColor = "green";
+// }
+// function answerIsWrong() {
+//     document.getElementById(currrentQuestionIndex).style.backgroundColor = "red";
+// }
+
+
+// function checkAnswer(answer) {
+//     if(questionsArr[currrentQuestionIndex].correct == answer) {
+//         score++;
+//         answerIsCorrect();
+//     } 
+//     else {
+//         answerIsWrong();
+//     }
+//     if(currrentQuestionIndex < lastQuestionIndex) {
+//         count = 0;
+//         currrentQuestionIndex++;
+//         questionRender();
+//     } 
+//     else {
+//         clearInterval(timer);
+//     }
+// }
+
+
 
 function startQuiz() {
-
-    qLead.innerHTML = questionsArr[0].question; 
     document.getElementById("playButton").style.display = "none";
-    document.getElementById("choice1").style.display = "inline";
-    document.getElementById("choice2").style.display = "inline";
-    document.getElementById("choice3").style.display = "inline";
-    document.getElementById("choice4").style.display = "inline";
-
+    document.getElementById("nextButton").classList.remove("hide");
+    
+    getQuestion();
 
     }
+
+function nextQuestion() {
+    currrentQuestionIndex++;
+}
+
 
 
 
